072b33c638cf9edf3e2499e4243c9033297242a8
shepherd <1059959730@qq.com>
2020-06-24 00:36:43 +0800

    add：添加发送短信工具类

---
+++ red-book-user/red-book-user-service/src/main/java/com/shepherd/redbookuserservice/utils/SendSmsUtil.java
@@ -0,0 +1,41 @@
+package com.shepherd.redbookuserservice.utils;
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2020/6/24 0:25
+ */
+
+public class SendSmsUtil {
+    public static void main(String[] args) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<accessKeyId>", "<accessSecret>");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", "17816875939");
+        request.putQueryParameter("SignName", "shepherd");
+        request.putQueryParameter("TemplateCode", "SMS_193518103");
+        request.putQueryParameter("TemplateParam", "{\"code\":\"655478\"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
