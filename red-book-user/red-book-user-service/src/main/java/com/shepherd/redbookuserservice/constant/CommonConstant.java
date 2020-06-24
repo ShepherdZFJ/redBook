54612a23fd38387ba33acef7004e270f10fd9b91
shepherd <zhengfj@isyscore.com>
2020-06-24 19:01:50 +0800

    add:添加常量类

---
+++ red-book-user/red-book-user-service/src/main/java/com/shepherd/redbookuserservice/constant/CommonConstant.java
@@ -0,0 +1,22 @@
+package com.shepherd.redbookuserservice.constant;
+
+/**
+ * @author fjZheng
+ * @version 1.0
+ * @date 2020/6/24 16:07
+ */
+public class CommonConstant {
+    /**
+     * 本机号码一键登录
+     */
+    Integer PHONE_LOCAL_LOGIN = 1;
+    /**
+     * 短信验证登录
+     */
+    Integer PHONE_MESSAGE_LOGIN = 2;
+    /**
+     * 账户密码登录
+     */
+    Integer USER_PASSWORD_LOGIN = 3;
+
+}
