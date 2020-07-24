5f0b09fbdc5e188d1b157bc2dada2efca3d05c1e
shepherd <zhengfj@isyscore.com>
2020-07-24 10:42:21 +0800

    add:添加时序数据库demo测试(与项目无关)

---
+++ red-book-user/red-book-user-service/src/main/java/com/shepherd/redbookuserservice/config/InfluxDbConfiguration.java
@@ -0,0 +1,47 @@
+package com.shepherd.redbookuserservice.config;
+
+/**
+ * @author fjZheng
+ * @version 1.0
+ * @date 2020/7/21 10:34
+ */
+import com.shepherd.redbookuserservice.connection.InfluxDbConnection;
+import org.influxdb.BatchOptions;
+import org.influxdb.InfluxDB;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(InfluxDB.class)
+public class InfluxDbConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.influx")
+    @ConditionalOnMissingBean(name = "influxDbProperties")
+    InfluxDbProperties influxDbProperties() {
+        return new InfluxDbProperties();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "influxDbConnection")
+    public InfluxDbConnection influxDbConnection(InfluxDbProperties influxDbProperties) {
+        BatchOptions batchOptions = BatchOptions.DEFAULTS;
+        batchOptions = batchOptions.actions(influxDbProperties.getActions());
+        batchOptions = batchOptions.flushDuration(influxDbProperties.getFlushDuration());
+        batchOptions = batchOptions.jitterDuration(influxDbProperties.getJitterDuration());
+        batchOptions = batchOptions.bufferLimit(influxDbProperties.getBufferLimit());
+
+        InfluxDbConnection influxDbConnection = new InfluxDbConnection(
+                influxDbProperties.getUrl(), influxDbProperties.getDatabase(), influxDbProperties.getRetentionPolicy(),
+                influxDbProperties.getRetentionPolicyTime(), batchOptions);
+        influxDbConnection.createRetentionPolicy();
+
+        System.out.println("init influxDb >>>>>>" + influxDbProperties);
+        return influxDbConnection;
+    }
+}
+
+
---
+++ red-book-user/red-book-user-service/src/main/java/com/shepherd/redbookuserservice/config/InfluxDbProperties.java
@@ -0,0 +1,22 @@
+package com.shepherd.redbookuserservice.config;
+
+import lombok.Data;
+
+/**
+ * @author fjZheng
+ * @version 1.0
+ * @date 2020/7/21 10:31
+ */
+@Data
+public class InfluxDbProperties {
+    private String url;
+//    private String userName;
+//    private String password;
+    private String database;
+    private String retentionPolicy = "autogen";
+    private String retentionPolicyTime = "30d";
+    private int actions = 2000;
+    private int flushDuration = 1000;
+    private int jitterDuration = 0;
+    private int bufferLimit = 10000;
+}
